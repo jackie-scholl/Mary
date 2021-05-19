8384ed7c8dc5e7a06dbd7fc97c1058ec37f0d9a9
Jackie Scholl <jackie.h.scholl@gmail.com>
2021-05-19 08:01:27 -0400

    lots of changes

--- justfile
+++ justfile
@@ -1,5 +1,20 @@
 build:
-	bnfc --haskell -o src Lambda.bnfc
+	time ~/.cabal/bin/bnfc --haskell -o src Lambda.bnfc
 	rm src/TestLambda.hs
-	stack build
-	
+	time stack build
+	time stack test
+
+qemu:
+	qemu-system-riscv64 \
+		-nographic \
+		-machine virt \
+		-smp 4 \
+		-m 2G \
+		-kernel risc-v-fedora/Fedora-Minimal-Rawhide-*-fw_payload-uboot-qemu-virt-smode.elf \
+		-bios none \
+		-object rng-random,filename=/dev/urandom,id=rng0 \
+		-device virtio-rng-device,rng=rng0 \
+		-device virtio-blk-device,drive=hd0 \
+		-drive file=risc-v-fedora/Fedora-Minimal-Rawhide-20200108.n.0-sda.raw,format=raw,id=hd0 \
+		-device virtio-net-device,netdev=usernet \
+		-netdev user,id=usernet,hostfwd=tcp::10000-:22	
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,44 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import Test.Chell
+import Interpreter
+import AbsLambda
+import ParLambda
+
+import qualified Data.Map.Strict
+
+tests_Math :: Suite
+tests_Math = suite "math"
+    [ test_Addition
+    , test_Subtraction
+    ]
+
+test_Addition :: Test
+test_Addition = assertions "addition" $ do
+    $expect (equal (2 + 1) 3)
+    $expect (equal (1 + 2) 3)
+
+test_Subtraction :: Test
+test_Subtraction = assertions "subtraction" $ do
+    $expect (equal (2 - 1) 1)
+    $expect (equal (1 - 2) (-1))
+
+tests_Mary :: Suite
+tests_Mary = suite "Mary"
+	[ test_idk
+	]
+
+test_idk :: Test
+test_idk = assertions "idk" $ do
+	$expect (equal (interpret "(~x. x)") (Right (DFunction (Ident "x") (EVar (Ident "x")) Data.Map.Strict.empty)))
+	$expect (equal (interpret "(let x 413 (let y 612 (let app (~x. (~y. x y)) (let const (~y. (~x. y)) ((app (const 1025)) 1111) ))))") (Right (DInt 1025)))
+	$expect (equal (interpret "(let app (~x. (~y. x y)) (let const (~y. (~x. y)) (let x 413 (let y 612 ((app (const 1025)) 1111) ))))") (Right (DInt 1025)))
+	$expect (equal (interpret "(and true false)") (Right (DBool False)))
+	$expect (equal (interpret "(and true 1)") (Right DError))
+	$expect (equal (interpret "(and true") (Left "syntax error at end of file"))
+	
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain [tests_Mary]
+
+--main :: IO ()
+--main = putStrLn "Test suite not yet implemented"
